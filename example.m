ef8cf91e843ae1e2003574aeefc2edf24862b64e
Marcus Billman <id23mbn@cs.umu.se>
2026-03-03 11:05:23 +0100

    Add missing plots, clean up comments, move output audio

--- example.m
+++ example.m
@@ -1,61 +1,63 @@
-%% STEG 1: LÄS IN LJUD OCH BERÄKNA STFT
+%% STEP 1: READ AUDIO AND CALCULATE STFT
 
 [x, Fs] = audioread('music/creep.mp3');
-x = x(:, 1); % Ta endast vänster kanal
+x = x(:, 1); % Use only left channel
 
 window = ones(2048,1);
 [X, w, t] = stft(x, Fs, "Window", window);
 
-%% STEG 2: GÖR 2DFT PÅ MAGNITUDEN AV STFT
+%% STEP 2: CALCULATE 2DFT OF STFT MAGNITUDE
 
 X_tilde = fft2(abs(X));
 
-%spectrogram(x, length(window), length(window)*0.5, 512, Fs, 'yaxis');
+%% STEG 3: DO PEAK-PICKING IN 2DFT
+%          -> BACKGROUND MASK
 
-%% STEG 3: GÖR PEAK-PICKING I 2DFT
-%          -> GER BAKGRUNDSMASK
-
-% Patch dimensions
+% Neighborhood dimensions
 patch_rows = 1;
 patch_cols = 15;
 
+% Create two matrices of the same size as X_tilde, where each element is
+% the max or min of the neighborhood centered around that element
 local_max = ordfilt2(abs(X_tilde), patch_rows*patch_cols, ones(patch_rows, patch_cols));
 local_min = ordfilt2(abs(X_tilde), 1, ones(patch_rows, patch_cols));
+
 alpha = local_max - local_min;
 center_is_max = abs(X_tilde) == local_max;
 
 gamma = std2(abs(X_tilde));
 % A point is included in the mask if it has high local contrast
-% OR if it's the dominant peak in its neighbourhood
+% OR if it's the dominant peak in its neighborhood
 mask_bg = (alpha > gamma) | center_is_max;
 
-%% STEG 4: INVERTERA BAKGRUNDSMASK
-%          -> GER FÖRGRUNDSMASK
+%% STEP 4: INVERT BACKGROUND MASK
+%          -> FOREGROUND MASK
 
 mask_fg = 1 - mask_bg;
 
-%% STEG 5: Multiplicera mask med 2DFT och gör I2DFT
-%          -> GER STFT FÖR BAKGRUND OCH FÖRGRUND
+%% STEG 5: APPLY MASK ON 2DFT AND CALCULATE I2DFT
+%          -> STFT FOR BACKGROUND AND FOREGROUND
 
-X_mag_bg = ifft2(mask_bg .* X_tilde); % TODO: try 'symmetric'
+X_mag_bg = ifft2(mask_bg .* X_tilde);
 X_mag_fg = ifft2(mask_fg .* X_tilde);
 
-%% STEG 6: GÖR ISTFT PÅ STFT FÖR BAKGRUND OCH FÖRGRUND
-%          -> Ger ljudsignaler för bakgrund och förgrund
+%% STEG 6: CALCULATE ISTFT OF STFT FOR BACKGROUND AND FOREGROUND
+%          -> AUDIO SIGNALS FOR BACKGROUND AND FOREGROUND
 
-phase_X = angle(X);  % original phase
+phase_X = angle(X);
 
 % Reconstruct complex STFT by combining with original phase
-X_bg = X_mag_bg .* exp(1j * phase_X); % j is the imaginary unit
-X_fg = X_mag_fg .* exp(1j * phase_X);
+% (1i is the imaginary unit)
+X_bg = X_mag_bg .* exp(1i * phase_X);
+X_fg = X_mag_fg .* exp(1i * phase_X);
 
-x_bg = real(istft(X_bg, Fs, "Window", window)); % TODO: try 'ConjugateSymmetric', true
-x_fg = real(istft(X_fg, Fs, "Window", window)); % instead of real()
+x_bg = real(istft(X_bg, Fs, "Window", window));
+x_fg = real(istft(X_fg, Fs, "Window", window));
 
-audiowrite("background.mp3", x_bg, Fs);
-audiowrite("foreground.mp3", x_fg, Fs);
+audiowrite("output/background.mp3", x_bg, Fs);
+audiowrite("output/foreground.mp3", x_fg, Fs);
 
-%% VISUALISERING
+%% VISUALIZATION
 
 tiny_number = 1e-7;
 
@@ -72,18 +74,26 @@ title("Mixture 2DFT");
 xlabel("Rate");
 ylabel("Scale");
 
+subplot(3, 3, [4 5]);
+imagesc(t, w, 20 * log10(abs(X_mag_bg) + tiny_number));
+title("Background spectrogram");
+xlabel("Time (s)");
+ylabel("Frequency (Hz)");
+
 subplot(3, 3, 6);
-imagesc(20 * log10(abs(X_mag_bg) + tiny_number));
-title("Background 2DFT");
+imagesc(mask_bg);
+title("Background mask 2DFT");
 xlabel("Rate");
 ylabel("Scale");
+
+subplot(3, 3, [7 8]);
+imagesc(t, w, 20 * log10(abs(X_mag_fg) + tiny_number));
+title("Foreground spectrogram");
+xlabel("Time (s)");
+ylabel("Frequency (Hz)");
 
 subplot(3, 3, 9);
-imagesc(20 * log10(abs(X_mag_fg) + tiny_number));
-title("Foreground 2DFT");
+imagesc(mask_fg);
+title("Foreground mask 2DFT");
 xlabel("Rate");
 ylabel("Scale");
-
-%subplot(3, 3, 2);
-%spectrogram(sound, length(window), length(window)*0.5, 512, Fs, 'yaxis');
-
